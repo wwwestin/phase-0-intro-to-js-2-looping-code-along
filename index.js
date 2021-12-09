170dbab03d36076d247fb32876016101c7516314
Westin Humble <westin.h@gmail.com>
2021-12-09 15:23:02 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+function writeCards(names) {
+    const cards = [];
+
+    for (let i = 0; i <= 2; i++) {
+        cards.push("Thank you, " + names[i] + ", for the wonderful surprise gift!");
+    }
+
+    return cards;
+};
+
+writeCards(names);
+
+
+function countDown() {
+    let number = 10;
+    while (number >= 0) {
+        console.log(number--);
+        debugger;
+    }
+};
+
+countDown(10);
